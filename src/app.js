39c1d6194a7e489d00664dc5b1fa265b94028dc0
azharadyatma <2210631170011@student.unsika.ac.id>
2024-11-29 15:08:30 +0700

    update deploy

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const adminAuthorization = require("./middleware/adminAuthorization");
 
-//const PORT = process.env.PORT;
-
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -25,8 +22,4 @@ app.use("/api/items", itemController);
 app.use("/api/users", adminAuthorization, userController);
 app.use("/api/transactions", transactionController);
 
-// app.listen(PORT, () => {
-//  console.log("App listening on port " + PORT);
-// });
-
-export default app;
+module.exports = app;
